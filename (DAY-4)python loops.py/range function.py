c9b2741d4340a4bd4d59e9e234d4a50061d8cfb4
Ahmed Tanveer <ahmedtanveer0326@gmail.com>
2025-08-08 12:57:13 -0700

    added

--- (DAY-4)python loops.py/range function.py
+++ (DAY-4)python loops.py/range function.py
@@ -4,4 +4,4 @@ print("\n")
 total=0
 for number in range(1,101):
  total+=number
- print(total)
+print(total)
---
+++ (DAY-5)while loop.py/while loop syntax.py
@@ -0,0 +1,4 @@
+response = ""
+while response.lower() != "yes":
+    response = input("Do you want to continue? Type 'yes' to proceed: ")
+print("Thank you!")
